56169ce5ff04b731bb641d0bc2ee4dbb52cfb317
etuhoha <etuhoha@gmail.com>
2026-05-13 10:30:21 +0300

    init rabbit connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/etuhoha/peril/internal/pubsub"
+	"github.com/etuhoha/peril/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	mqConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("could not connect to MQ: %v", err)
+	}
+	defer mqConnection.Close()
+	fmt.Println("Connected to MQ...")
+
+	mqChannel, err := mqConnection.Channel()
+	if err != nil {
+		log.Fatalf("could not create MQ channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(mqChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("could not send to MQ channel: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Printf("Stopping Peril server...")
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,18 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	bytes, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	pub := amqp.Publishing{ContentType: "application/json", Body: bytes}
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, pub)
+}
