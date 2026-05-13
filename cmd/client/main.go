4dea32f091aab1c3b95fbdb06868559d72c81d0b
etuhoha <etuhoha@gmail.com>
2026-05-13 11:00:17 +0300

    clien pause queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/etuhoha/peril/internal/gamelogic"
+	"github.com/etuhoha/peril/internal/pubsub"
+	"github.com/etuhoha/peril/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	mqConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("could not connect to MQ: %v", err)
+	}
+	defer mqConnection.Close()
+	fmt.Println("Connected to MQ...")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("could not log in: %v", err)
+	}
+
+	queueName := fmt.Sprintf("%v.%v", routing.PauseKey, username)
+	pubsub.DeclareAndBind(mqConnection, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.QueueTypeTransient)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -16,3 +16,36 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	pub := amqp.Publishing{ContentType: "application/json", Body: bytes}
 	return ch.PublishWithContext(context.Background(), exchange, key, false, false, pub)
 }
+
+type SimpleQueueType int
+
+const (
+	QueueTypeDurable = iota
+	QueueTypeTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := queueType == QueueTypeDurable
+	queue, err := channel.QueueDeclare(queueName, durable, !durable, !durable, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+}
