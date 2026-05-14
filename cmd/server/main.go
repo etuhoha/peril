8cf13cb2ede0c2a1e2d6e3b74a169fe92d0c8cd6
etuhoha <etuhoha@gmail.com>
2026-05-14 11:11:28 +0300

    server: sending pause/unpause

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/etuhoha/peril/internal/gamelogic"
 	"github.com/etuhoha/peril/internal/pubsub"
 	"github.com/etuhoha/peril/internal/routing"
 
@@ -28,14 +27,31 @@ func main() {
 		log.Fatalf("could not create MQ channel: %v", err)
 	}
 
-	err = pubsub.PublishJSON(mqChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-	if err != nil {
-		log.Fatalf("could not send to MQ channel: %v", err)
+	gamelogic.PrintServerHelp()
+
+	for {
+		input := gamelogic.GetInput()
+		cmd := input[0]
+		switch cmd {
+		case "pause":
+			fmt.Printf("Sending pause message...\n")
+			err = pubsub.PublishJSON(mqChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Fatalf("could not send 'pause': %v", err)
+			}
+		case "resume":
+			fmt.Printf("Sending resume message...\n")
+			err = pubsub.PublishJSON(mqChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Fatalf("could not send 'resume': %v", err)
+			}
+		case "quit":
+			goto exit
+		default:
+			fmt.Printf("unknown command '%v'\n", cmd)
+		}
 	}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-
-	fmt.Printf("Stopping Peril server...")
+exit:
+	fmt.Printf("Stopping Peril server...\n")
 }
